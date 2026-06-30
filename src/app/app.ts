851a335c4a0ed4c56fb9589f1171e9613a1147af
jnswkz <23520408@gm.uit.edu.vn>
2026-06-30 15:41:46 +0700

    initial frontend

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SidebarNav } from './sidebar-nav/sidebar-nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MatSidenavModule, MatToolbarModule, SidebarNav],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('assetsMgmt-fe');
-}
+export class App {}
